4788fdf757c8220887b7e8aa733d19cdaaac090d
Ryo ARIMA <ryo.arima@zohomail.jp>
2025-10-23 07:18:20 +0900

    refactor: update environment command aliases and streamline repository structure

--- src/ent/request/environment.rs
+++ src/ent/request/environment.rs
@@ -1,1 +1,7 @@
+#![allow(non_camel_case_types)]
 
+pub struct ENVIRONMENT {
+    pub name: String,
+    pub description: Option<String>,
+    pub tags: Vec<String>,
+}
--- src/ent/response/environment.rs
+++ src/ent/response/environment.rs
@@ -1,1 +1,10 @@
 
+#![allow(non_camel_case_types)]
+
+use crate::ent::model::environment::ENVIRONMENT as ModelEnvironment;
+
+pub struct ENVIRONMENT {
+    pub mcode: String,
+    pub messages: String,
+    pub environment: ModelEnvironment
+}
